0bc5463c48696b6437217b5f815af2375de2f99f
Анжелика <a.vengrzhinovskaya@gmail.com>
2022-04-23 21:22:35 +0700

    #4

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.androidintern
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_ok)
+        button.setOnClickListener {
+            Toast.makeText(applicationContext, "Ok pressed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
