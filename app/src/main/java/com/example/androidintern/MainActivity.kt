30a29a32a511c07e7c20e33dcee803fabe9284ae
Анжелика <a.vengrzhinovskaya@gmail.com>
2022-04-28 22:22:43 +0700

    #5

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.androidintern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_edit)
+        val edit = findViewById<EditText>(R.id.edit_text)
+
+        button.setOnClickListener {
+            Log.i("text_info", edit.text.toString())
+            Timber.i(edit.text.toString())
+        }
     }
 }
