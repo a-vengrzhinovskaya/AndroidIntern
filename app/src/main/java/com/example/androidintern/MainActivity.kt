4f7fa4dc1d6f36e95f648364b9c645ea971307a5
Анжелика <a.vengrzhinovskaya@gmail.com>
2022-05-01 18:34:42 +0700

    #9

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.androidintern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.text_view)
+        val checkBox = findViewById<CheckBox>(R.id.checkbox)
+        val editText = findViewById<EditText>(R.id.edit_text)
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+
+        button.setOnClickListener {
+            if (checkBox.isChecked) {
+                textView.text = editText.text
+                progressBar.progress += 10
+
+                if (progressBar.progress == progressBar.max) {
+                    progressBar.progress = 0
+                }
+            }
+        }
     }
 }
