fad8dae72ab632a5461ef699eb44ce4476c00926
Анжелика <a.vengrzhinovskaya@gmail.com>
2022-05-09 01:08:57 +0700

    #10

--- app/src/main/java/com/example/androidintern/ColorAdapter.kt
+++ app/src/main/java/com/example/androidintern/ColorAdapter.kt
@@ -6,19 +6,29 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-data class ColorAdapter(private val colorNames: List<String>) : RecyclerView.Adapter<ColorAdapter.ColorViewHolder>() {
-    class ColorViewHolder(item: View) : RecyclerView.ViewHolder(item) {
-        val textView = item.findViewById<TextView>(R.id.recycler_text_view)
-    }
-
+class ColorAdapter(private var colorNames: List<String>) :
+    RecyclerView.Adapter<ColorViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorViewHolder {
         val item = LayoutInflater.from(parent.context).inflate(R.layout.color_item, parent, false)
         return ColorViewHolder(item)
     }
 
     override fun onBindViewHolder(holder: ColorViewHolder, position: Int) {
-        holder.textView.text = colorNames[position]
+        holder.bind(colorNames[position])
     }
 
     override fun getItemCount() = colorNames.size
+
+    fun setColors(newColors: List<String>) {
+        colorNames = newColors
+        notifyDataSetChanged()
+    }
+}
+
+class ColorViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val textView = view.findViewById<TextView>(R.id.recycler_text_view)
+
+    fun bind(item: String) {
+        textView.text = item
+    }
 }
--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -6,12 +6,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val adapter = ColorAdapter(emptyList())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        adapter.setColors(resources.getStringArray(R.array.color_names).toList())
+        initRV()
+    }
+
+    private fun initRV() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = ColorAdapter(resources.getStringArray(R.array.color_names).toList())
+        recyclerView.adapter = adapter
     }
 }
