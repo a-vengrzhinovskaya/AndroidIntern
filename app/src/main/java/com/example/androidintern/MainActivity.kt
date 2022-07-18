e1574800b2fd4c254686372113103f527bfec653
Анжелика <a.vengrzhinovskaya@gmail.com>
2022-07-18 19:40:11 +0700

    исправила

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -4,34 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.concurrent.thread
+import timber.log.Timber
+
+val url =
+    URL("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val url =
-            URL("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+        Timber.plant(Timber.DebugTree())
 
-        Thread {
-            getData(url)
-        }.start()
+        getData(url)
     }
 
     private fun getData(url: URL) {
-        val connection = url.openConnection() as HttpURLConnection
-        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-            try {
-                val data = connection.inputStream.bufferedReader().readText()
-                println(data)
+        thread {
+            val connection = url.openConnection() as HttpURLConnection
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                try {
+                    val data = connection.inputStream.bufferedReader().readText()
+                    Timber.i(data)
 
-            } catch (exception: Exception) {
-                println(exception.message)
-            } finally {
-                connection.disconnect()
+                } catch (exception: Exception) {
+                    Timber.i(exception.message)
+                } finally {
+                    connection.disconnect()
+                }
+            } else {
+                Timber.i("Error")
             }
-        } else {
-            println("Error")
         }
     }
 }
