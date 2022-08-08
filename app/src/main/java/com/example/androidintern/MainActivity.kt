fa5822b8f1cfb4913fd0de15e4e6971cf30b48ae
Анжелика <a.vengrzhinovskaya@gmail.com>
2022-08-08 18:11:18 +0700

    #13

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.androidintern
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
+import java.net.URL
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+
+private const val CURRENT_URL =
+    "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
+private const val MY_TAG = "tag"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getData()
+    }
+
+    private fun getData() {
+        val url = URL(CURRENT_URL)
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+        client.newCall(request).enqueue(
+            object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e(MY_TAG, e.toString())
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val data = response.body!!.string()
+                        Log.d(MY_TAG, data)
+                    }
+                }
+            }
+        )
     }
 }
