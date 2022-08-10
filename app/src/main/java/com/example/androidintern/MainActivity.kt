b8869c6d161715d6a2289b8cc7825e29906ffb52
wdkokskwpqo <a.vengrzhinovskaya@gmail.com>
2022-08-10 15:17:38 +0700

    #14

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidintern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.androidintern.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btOK.setOnClickListener {
+            binding.tvText.text = getString(R.string.ok)
+        }
     }
 }
