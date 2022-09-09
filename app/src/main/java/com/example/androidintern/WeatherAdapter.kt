8ccf3228cc0c7e1f68fd148b26239f5abc694dea
Анжелика <a.vengrzhinovskaya@gmail.com>
2022-09-09 21:46:33 +0700

    #25

--- app/src/main/java/com/example/androidintern/WeatherAdapter.kt
+++ app/src/main/java/com/example/androidintern/WeatherAdapter.kt
@@ -6,24 +6,65 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.androidintern.databinding.ItemWeatherHolderBinding
+import com.example.androidintern.databinding.ItemColdWeatherHolderBinding
+import com.example.androidintern.databinding.ItemWarmWeatherHolderBinding
 
 private const val ICON_URL = "https://openweathermap.org/img/wn/"
+private const val TEMPERATURE = 10
+private const val TYPE_WARM = 1
+private const val TYPE_COLD = 2
 
 class NumberAdapter :
-    ListAdapter<WeatherNW.WeatherData, WeatherViewHolder>(WeatherItemDiffCallback) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
-        val binding =
-            ItemWeatherHolderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return WeatherViewHolder(binding)
+    ListAdapter<WeatherNW.WeatherData, RecyclerView.ViewHolder>(WeatherItemDiffCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == TYPE_WARM) {
+            val binding =
+                ItemWarmWeatherHolderBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            return WarmWeatherViewHolder(binding)
+        } else {
+            val binding =
+                ItemColdWeatherHolderBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            return ColdWeatherViewHolder(binding)
+        }
     }
 
-    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
-        holder.bind(getItem(position))
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is WarmWeatherViewHolder -> holder.bind(getItem(position))
+            is ColdWeatherViewHolder -> holder.bind(getItem(position))
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (getItem(position).main.temp < TEMPERATURE) {
+            return TYPE_COLD
+        }
+        return TYPE_WARM
     }
 }
 
-class WeatherViewHolder(private val binding: ItemWeatherHolderBinding) :
+class WarmWeatherViewHolder(private val binding: ItemWarmWeatherHolderBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(weather: WeatherNW.WeatherData) {
+        binding.tvDate.text = weather.dtTxt
+        Glide
+            .with(binding.root)
+            .load("$ICON_URL${weather.weather.first().icon}.png")
+            .into(binding.ivWeatherIcon)
+        binding.tvPressure.text = weather.main.pressure.toString()
+        binding.tvTemperature.text = weather.main.temp.toString()
+    }
+}
+
+class ColdWeatherViewHolder(private val binding: ItemColdWeatherHolderBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(weather: WeatherNW.WeatherData) {
         binding.tvDate.text = weather.dtTxt
@@ -37,9 +78,15 @@ class WeatherViewHolder(private val binding: ItemWeatherHolderBinding) :
 }
 
 object WeatherItemDiffCallback : DiffUtil.ItemCallback<WeatherNW.WeatherData>() {
-    override fun areItemsTheSame(oldItem: WeatherNW.WeatherData, newItem: WeatherNW.WeatherData): Boolean =
+    override fun areItemsTheSame(
+        oldItem: WeatherNW.WeatherData,
+        newItem: WeatherNW.WeatherData
+    ): Boolean =
         oldItem == newItem
 
-    override fun areContentsTheSame(oldItem: WeatherNW.WeatherData, newItem: WeatherNW.WeatherData): Boolean =
+    override fun areContentsTheSame(
+        oldItem: WeatherNW.WeatherData,
+        newItem: WeatherNW.WeatherData
+    ): Boolean =
         oldItem == newItem
 }
